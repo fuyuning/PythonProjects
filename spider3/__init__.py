a518b7e056d262daa1113f283b6c5b54c0124997
fuyuning <46981955+fuyuning@users.noreply.github.com>
2019-01-29 17:42:21 +0800

    Add files via upload

---
+++ spider3/__init__.py
@@ -0,0 +1,8 @@
+#Author     :LuciferMorningStar
+#-*- coding:utf8 -*
+#@Time      :19-1-29 
+#@Author    :LuciferMorningStar
+#@Site      :
+#@file      :__init__.py.py
+#@Software  :PyCharm
+name = "spider2"
